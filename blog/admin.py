7e0a340723974ec7a59d82afd56f4c2b6e7dd0c0
Hasan Akalp <hasan.akalp@gmail.com>
2019-01-09 16:10:47 +0300

    Models are registered to Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Post, Comment, Topic
 
-# Register your models here.
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Topic)
